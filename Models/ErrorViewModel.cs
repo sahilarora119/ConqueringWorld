c3e693e00a1491d461b06dd732d541ca41268226
Sahil <sahil.mlk769@gmail.com>
2024-11-06 02:09:16 -0500

    Inital Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ConqueringWorld.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
